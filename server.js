5563a58aaf8f3a9b5a1899088f74a63ef54bd37d
StanimalTheMan <sjc591@nyu.edu>
2020-03-25 14:49:47 -0400

    fix database settings on heroku

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const cors = require("cors");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
     /*
     UNCOMMENT FOR DEVELOPMENT RUN
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
